aa439b37948cd9ff112be7c7d87c5b5a8121b4a5
hydraChaosu <abmallim@gmail.com>
2019-05-21 14:51:30 +0200

    Add bound actions

--- src/redux/todo/actions.js
+++ src/redux/todo/actions.js
@@ -1,5 +1,18 @@
 import types from "./types";
+i;
 
-const add = item => ({ type: types.ADD_TODO, item });
-const remove = item => ({ type: types.REMOVE_TODO, item });
-const complete = item => ({ type: types.COMPLETE_TODO, item });
+const add = text => ({ type: types.ADD_TODO, text });
+const remove = index => ({ type: types.REMOVE_TODO, index });
+const complete = index => ({ type: types.COMPLETE_TODO, index });
+
+const boundAdd = dispatch(add(text));
+const boundRemove = dispatch(remove(index));
+const boundComplete = dispatch(complete(index));
+export default {
+  add,
+  remove,
+  complete,
+  boundAdd,
+  boundComplete,
+  boundRemove
+};
