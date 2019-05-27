aaf1a025864aa97792c4330c19d7585fe4fba5ab
hydraChaosu <abmallim@gmail.com>
2019-05-27 10:12:49 +0200

    'nochange'

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import useInput from "../hooks/hookInput";
+import { connect } from "react-redux";
+import add from "../redux/todo/actions";
 const AddTodo = () => {
   useEffect(() => {
     input.focus();
@@ -16,6 +18,8 @@ const AddTodo = () => {
       onSubmit={e => {
         e.preventDefault();
         reset();
+        if (value === "") return;
+        // dispatch(add(value));
       }}
     >
       <input type="text" {...bind} ref={ev => (input = ev)} />
@@ -24,4 +28,4 @@ const AddTodo = () => {
   );
 };
 
-export default AddTodo;
+export default connect()(AddTodo);
